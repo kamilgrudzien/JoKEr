b070104e786ecfcc0b0275601529222da23f186e
Kamil Grudzien <kamilgrudzien4@gmail.com>
2021-10-09 10:33:47 -0500

    Update JoKEr.py
    added ip_address function and copy_files function

--- JoKEr.py
+++ JoKEr.py
@@ -2,6 +2,9 @@ import os
 import sys
 import ctypes
 import time
+import shutil
+import socket
+import subprocess
 #using ctypes for wallpaper changes
 #time we can use for delays and cycling changes
 
@@ -14,7 +17,20 @@ img1 = r"C:\Users\Kamil\Desktop\v33r1.png"
 #need to update this to not include the absolute path
 def background():
     #SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, img1, 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, img, 0)
+
+def ip_address():
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    f = open("Ip_address.txt", "a")
+    print("Your attacking Computer Name is:" + hostname, file=f)
+    print("Your attacking Computer IP Address is:" + IPAddr, file=f)
+    f.close()    
+
+def copy_files(sourceFolder, targetFolder):
+
+    shutil.copytree(sourceFolder, targetFolder)
+    print("files copied")
     
 
 def main():
